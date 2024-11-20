549480517fd608c71cf02dc8f56de83e6b8df279
NobuTajima <ntajima@cs.takushoku-u.ac.jp>
2024-11-20 10:45:57 +0900

    L2 田島 修正

--- ap08L2.js
+++ ap08L2.js
@@ -19,6 +19,10 @@ let course;
 export const origin = new THREE.Vector3();
 export const controlPoints = [
     [ 50,-20],
+    //追加 スライド15 By Tajima
+    [ 25,-20],
+    [ 25, 20],
+    [-25, 20],   
     [-25, 40]
 ]
 export function init(scene, size, id, offset, texture) {
@@ -45,14 +49,88 @@ export function init(scene, size, id, offset, texture) {
     plane.position.set(offset.x, -0.01, offset.z);
     scene.add(plane);
 
-    // ビル
+    // ビル  スライド19、20  By Tajima
+    function makeBuilding(x, z, type) {
+        const height = [2, 2, 7, 4, 5];
+        const bldgH = height[type]*5;
+        const geometry = new THREE.BoxGeometry(8, bldgH, 8);
+        const material = new THREE.MeshLambertMaterial({map: texture});
+        const sideUvS = (type*2+1)/11;
+        const sideUvE = (type*2+2)/11;
+        const topUvS = (type*2+2)/11;
+        const topUvE = (type*2+3)/11;
+        const uvs = geometry.getAttribute("uv");
+        for (let i = 0; i < 48; i+=4) {
+            if (i < 16 || i > 22) {
+                uvs.array[i] = sideUvS;
+                uvs.array[i+2] = sideUvE;
+            }
+            else {
+                uvs.array[i] = topUvS;
+                uvs.array[i+2] = topUvE;
+            }
+        }
+        const bldg = new THREE.Mesh(
+            geometry,
+            material
+        )
+        bldg.position.set(offset.x + x, bldgH/2, offset.z + z);
+        scene.add(bldg);
+    }
+    makeBuilding(10, 0, 2);
+    makeBuilding( 0, 0, 2);
+    makeBuilding(-10, 0, 2);
 
-    // コース(描画)
 
+    // コース(描画)  スライド14 by Tajima
+    //制御点を補完して曲線を作る
+    const course = new THREE.CatmullRomCurve3(
+        controlPoints.map((p) => {
+            return (new THREE.Vector3()).set(
+                offset.x + p[0],
+                0,
+                offset.z + p[1]
+            );
+        }), false
+    )
+        
+    //曲線から100箇所を取り出し、円を並べる
+    const points = course.getPoints(100);
+    points.forEach((point)=>{
+        const road = new THREE.Mesh(
+            new THREE.CircleGeometry(5,16),
+            new THREE.MeshLambertMaterial({color: "gray"})
+        )
+        road.rotateX(-Math.PI/2);
+        road.position.set(
+            point.x,
+            0,
+            point.z
+        );
+        scene.add(road);
+    });
+    
 }
 
-// コース(自動運転用)
+// コース(自動運転用) スライド18 By Tajima
 export function makeCourse(scene) {
+    const courseVectors = [];
+    const parts = [L2, L3, L4, L1];
+    parts.forEach((part) => {
+        part.controlPoints.forEach((p) => {
+            courseVectors.push(
+                new THREE.Vector3(
+                    p[0] + part.origin.x,
+                    0,
+                    p[1] + part.origin.z,
+                )
+            )
+        });
+    })
+    course = new THREE.CatmullRomCurve3(
+        courseVectors, true
+    )
+
 }
 
 // カメラを返す
@@ -60,8 +138,12 @@ export function getCamera() {
     return camera;
 }
 
-// 車の設定
+// 車の設定  スライド12 by Tajima
 export function setCar(scene, car) {
+    const SCALE = 0.01;
+    car.position.copy(origin);
+    car.scale.set(SCALE, SCALE, SCALE);
+    scene.add(car);
 }
 
 // Windowサイズの変更処理
@@ -71,7 +153,16 @@ export function resize() {
     renderer.setSize(sizeR, sizeR);
 }
 
+//描画　スライド17 By Tajima
+const clock = new THREE.Clock();
+const carPosition = new THREE.Vector3();
+const carTarget = new THREE.Vector3();
 export function render(scene, car) {
+    const time = (clock.getElapsedTime() / 20);
+    course.getPointAt(time % 1, carPosition);
+    car.position.copy(carPosition);
+    course.getPointAt((time + 0.01) %1, carTarget);
+    car.lookAt(carTarget);
     camera.lookAt(car.position.x, car.position.y, car.position.z);
     renderer.render(scene, camera);
 }
